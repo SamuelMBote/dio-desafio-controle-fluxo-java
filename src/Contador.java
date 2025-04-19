3d9ac2df544cb2d8b24c73fe93a840bd88acd0e4
IntegraseOnline <36938470+SamuelMBote@users.noreply.github.com>
2025-04-19 12:30:48 -0300

    Resultado do Desafio

---
+++ src/Contador.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Insira o primeiro valor");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Insira o segundo valor");
+        int parametroDois = terminal.nextInt();
+        System.out.println("---");
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parametro deve ser maior que o primeiro");
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroDois <= parametroUm) {
+            throw new ParametrosInvalidosException();
+        }
+        for (int index = parametroUm; index <= parametroDois; index++) {
+            System.out.println(index);
+        }
+
+    }
+}
